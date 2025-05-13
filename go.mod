661db84805ea38dbcc9bce5e452b2555b49f7567
John <gtom_1984@hotmail.ca>
2025-05-12 17:12:29 -0700

    Setup Basic JWT methods
    Use the JWT tokens methods in the course to perform user authentication

--- go.mod
+++ go.mod
@@ -4,13 +4,16 @@ go 1.23.4
 
 require (
 	github.com/BurntSushi/toml v1.4.1-0.20240526193622-a339e1f7089c // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.2 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	golang.org/x/crypto v0.38.0 // indirect
 	golang.org/x/exp/typeparams v0.0.0-20241210194714-1829a127f884 // indirect
 	golang.org/x/mod v0.23.0 // indirect
-	golang.org/x/sync v0.11.0 // indirect
-	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/sync v0.14.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
 	golang.org/x/telemetry v0.0.0-20241220003058-cc96b6e0d3d9 // indirect
-	golang.org/x/text v0.22.0 // indirect
+	golang.org/x/text v0.25.0 // indirect
 	golang.org/x/tools v0.30.1-0.20250221230316-5055f70f240c // indirect
 	golang.org/x/tools/gopls v0.18.1 // indirect
 	golang.org/x/vuln v1.1.3 // indirect
---
+++ internal/auth/auth_methods.go
@@ -0,0 +1,76 @@
+package auth
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+// --- Hashing ---
+
+// Hash passwords using Bcrypt
+func HashPassword(password string) (string, error) {
+	passwordBytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.MinCost)
+	if err != nil {
+		return "", fmt.Errorf("ERROR: Unable to generate password")
+	}
+	return string(passwordBytes), nil
+}
+
+// --- JSON Web Tokens ---
+
+// Creating a JWT
+func CreateJWT(userID uuid.UUID, secret string, issuer string, expiry time.Duration) (string, error) {
+
+	// Creating a registered claims
+	claims := jwt.RegisteredClaims{
+		Issuer:    issuer,
+		IssuedAt:  jwt.NewNumericDate(time.Now()),
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiry)),
+		Subject:   userID.String(),
+	}
+
+	// Creating a token object with claims
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	// Returning token with secret
+	return token.SignedString([]byte(secret))
+}
+
+func ValidateJWT(tokenString, secret string) (uuid.UUID, error) {
+	type claimsStruct struct {
+		jwt.RegisteredClaims
+	}
+
+	var userID uuid.UUID
+
+	// Parsing the Token to ensure it is signed correctly
+	extractedToken, err := jwt.ParseWithClaims(tokenString, &claimsStruct{}, func(token *jwt.Token) (interface{}, error) {
+		// Type assserting to ensure that the token's Method is of SigningMethodHMAC
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("ERROR: Unexpected signing method %v", token.Header["alg"])
+		}
+		return []byte(secret), nil
+	})
+
+	if err != nil {
+		return userID, fmt.Errorf("ERROR: Unable to parse token: %v", err)
+	}
+
+	// Checking if we can get Subject Field
+	userIDStr, err := extractedToken.Claims.GetSubject()
+	if err != nil {
+		return userID, fmt.Errorf("ERROR: Unable to get Subject field from token: %v", err)
+	}
+
+	// Checking if we can parse the Subject Field into a UUID
+	userID, err = uuid.Parse(userIDStr)
+	if err != nil {
+		return userID, fmt.Errorf("ERROR: Unable to parse %s to uuid.UUID: %v", userIDStr, err)
+	}
+
+	return userID, nil
+}
