c4becac1ea9d49059f79d8586c71538d97009f3e
John <gtom_1984@hotmail.ca>
2025-05-16 12:07:52 -0700

    Start working on Server responses
    Create an error and json response functions. Also developed a CreateUser
    SQL Query

--- go.mod
+++ go.mod
@@ -3,23 +3,9 @@ module github.com/megarage9000/Prayer-Buddies
 go 1.23.4
 
 require (
-	github.com/BurntSushi/toml v1.4.1-0.20240526193622-a339e1f7089c // indirect
-	github.com/golang-jwt/jwt/v5 v5.2.2 // indirect
-	github.com/google/go-cmp v0.6.0 // indirect
-	github.com/google/uuid v1.6.0 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
-	github.com/lib/pq v1.10.9 // indirect
-	golang.org/x/crypto v0.38.0 // indirect
-	golang.org/x/exp/typeparams v0.0.0-20241210194714-1829a127f884 // indirect
-	golang.org/x/mod v0.23.0 // indirect
-	golang.org/x/sync v0.14.0 // indirect
-	golang.org/x/sys v0.33.0 // indirect
-	golang.org/x/telemetry v0.0.0-20241220003058-cc96b6e0d3d9 // indirect
-	golang.org/x/text v0.25.0 // indirect
-	golang.org/x/tools v0.30.1-0.20250221230316-5055f70f240c // indirect
-	golang.org/x/tools/gopls v0.18.1 // indirect
-	golang.org/x/vuln v1.1.3 // indirect
-	honnef.co/go/tools v0.5.1 // indirect
-	mvdan.cc/gofumpt v0.7.0 // indirect
-	mvdan.cc/xurls/v2 v2.5.0 // indirect
+	github.com/golang-jwt/jwt/v5 v5.2.2
+	github.com/google/uuid v1.6.0
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
+	golang.org/x/crypto v0.38.0
 )
--- server.go
+++ server.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 )
 
@@ -9,4 +11,34 @@ func helloWorld(resp http.ResponseWriter, req *http.Request) {
 	resp.Header().Set("Content-Type", "application/json")
 	resp.WriteHeader(http.StatusOK)
 	resp.Write([]byte(helloWorld))
+}
+
+// Function to return JSON
+func (config *Config) RespondJSON(resp http.ResponseWriter, req *http.Request, payload interface{}, statusCode int) {
+	ConfigureResponse(resp, statusCode, payload)
+}
+
+// Function to return error response
+func RespondError(resp http.ResponseWriter, req *http.Request, statusCode int, errorMessage string) {
+	errorResp := struct {
+		ErrorMessage string `json: "errorMessage"`
+	}{
+		ErrorMessage: errorMessage,
+	}
+
+	ConfigureResponse(resp, statusCode, errorResp)
+}
+
+// Function to configure response
+func ConfigureResponse(resp http.ResponseWriter, statusCode int, payload interface{}) {
+	data, err := json.Marshal(payload)
+	if err != nil {
+		fmt.Printf("ERROR: unable to marshal data for a response")
+		return
+	}
+
+	// Configuring the response
+	resp.Header().Set("Content-Type", "application/json")
+	resp.WriteHeader(statusCode)
+	resp.Write(data)
 }
